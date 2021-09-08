878deb89ee09caefce56534fc07f1eeab3f8d6bf
lianglva <18651954296@163.com>
2021-09-08 16:54:13 +0800

    message:chown and chmod userhome author:lvliang

---
+++ daily_operation_maintenance/chmod_chown_userhome.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#******************************************* #
+# 文件名：chmod_chown_userhome.sh      	     #
+# 作  者： lvliang                           #
+# 日  期：2021年9月8日                       #
+# 最后修改：2021年9月8日                     #
+# 功  能：应用用户家目录权限修改			 #
+# 操作系统：linux							 #
+# 复核人：                                   #
+#********************************************#
+#用户名
+USER_NAME="lvliang"
+
+#家目录权限，比如755，如果不填则不修改
+AUTHORRITY=""
+
+#家目录属组和属主，比如root:root ,如果不填则不修改
+GROUP_USER=""
+
+function check_user()
+{
+	id ${USER_NAME} > /dev/null 2>&1
+	if [ $? -ne 0 ];then
+		echo "user ${USER_NAME} does not exists." && return 1
+	fi
+}
+function check_userhome()
+{
+	local userhome="/home/${USER_NAME}"
+	if [ ! -d /home/${USER_NAME} ];then
+		echo "userhome /home/${USER_NAME} does not exists." && return 1
+	fi
+}
+function do_chown()
+{
+	if [ "x${GROUP_USER}" = "x" ];then
+		return 0
+	fi
+	chown -R ${GROUP_USER} /home/${USER_NAME}
+	if [ $? -ne 0 ];then
+		echo "chown -R ${GROUP_USER} /home/${USER_NAME} failed." && return 1
+	fi
+}
+function do_chmod()
+{
+	if [ "x${AUTHORRITY}" = "x" ];then
+		return 0
+	fi
+	chmod ${AUTHORRITY} /home/${USER_NAME}
+	if [ $? -ne 0 ];then
+		echo "chmod ${AUTHORRITY} /home/${USER_NAME} failed." && return 1
+	fi
+}
+function main()
+{
+	#检查用户是否存在
+	check_user
+	if [ $? -ne 0 ];then
+		echo "check_user failed." && return 1
+	fi
+	#检查家目录是否存在
+	check_userhome
+	if [ $? -ne 0 ];then
+		echo "check_userhome failed." && return 1
+	fi
+	#修改属组和属主
+	do_chown
+	if [ $? -ne 0 ];then
+		echo "do_chown failed." && return 1
+	fi
+	#修改家目录权限
+	do_chmod
+	if [ $? -ne 0 ];then
+		echo "do_chmod failed." && return 1
+	fi
+}
+main $@
